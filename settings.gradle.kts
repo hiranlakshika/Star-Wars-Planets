de26172523234e721a1cc5b70afb9039fe2e6122
Hiran Lakshika <hiranlakshika@gmail.com>
2025-07-16 14:03:34 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Star Wars Planets"
+include(":app")
+include(":core:common")
+include(":core:network")
+include(":feature")
+include(":feature:planets")
+include(":feature:planetdetails")
