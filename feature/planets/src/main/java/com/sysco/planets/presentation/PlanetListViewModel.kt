b89cbf5872e6a53c88ea3e5972bc9b8c6f98e819
Hiran Lakshika <hiranlakshika@gmail.com>
2025-07-17 21:36:51 +0200

    offline List loading

--- feature/planets/src/main/java/com/sysco/planets/presentation/PlanetListViewModel.kt
+++ feature/planets/src/main/java/com/sysco/planets/presentation/PlanetListViewModel.kt
@@ -8,8 +8,10 @@ import com.sysco.shared.core.domain.model.Result
 import com.sysco.shared.core.presentation.UiText
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -34,6 +36,11 @@ class PlanetListViewModel @Inject constructor(private val planetsRepository: Pla
     }
 
     private fun fetchPlanets() = viewModelScope.launch(Dispatchers.IO) {
+        async { getRemotePlanets() }.await()
+        getLocalPlanets()
+    }
+
+    private suspend fun getRemotePlanets() {
         _state.update { it.copy(isLoading = true) }
 
         when (val result = planetsRepository.getPlanets()) {
@@ -50,6 +57,7 @@ class PlanetListViewModel @Inject constructor(private val planetsRepository: Pla
                 _state.update {
                     it.copy(
                         isLoading = false,
+                        error = null,
                         planets = result.data
                     )
                 }
@@ -57,4 +65,28 @@ class PlanetListViewModel @Inject constructor(private val planetsRepository: Pla
         }
     }
 
+    private suspend fun getLocalPlanets() {
+        planetsRepository.getLocalPlanets().collectLatest { result ->
+            when (result) {
+                is Result.Error -> {
+                    _state.update {
+                        it.copy(
+                            isLoading = false,
+                            error = UiText.StringResource(R.string.failed_to_load_data)
+                        )
+                    }
+                }
+
+                is Result.Success -> {
+                    _state.update {
+                        it.copy(
+                            isLoading = false,
+                            error = null,
+                            planets = result.data
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
